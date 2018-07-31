f9a0c675579462f90b8f04cb007c17940659c0bf
cacadosman <wetmanz23@gmail.com>
2018-07-31 23:24:50 +0700

    fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -17,6 +17,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+	\URL::forceScheme('https');
+	if($this->app->environment() === 'production'){ $this->app['request']->server->set('HTTPS', true); }
         // Database
         Schema::defaultStringLength(191);
 
