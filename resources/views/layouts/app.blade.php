c8065dad584e49781551e0361a0fd93309f0caf3
cacadosman <wetmanz23@gmail.com>
2018-08-03 08:47:17 +0700

    a

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="{{ config('app.locale') }}">
 <head>
-    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}" >	
+    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}" >
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
