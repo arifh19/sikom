b9b4b723f1472c7d2d89e4d3eb4571e8b65536f1
Arif Hidayat <arif.h@mail.ugm.ac.id>
2018-08-06 12:50:06 +0700

    admin

--- resources/views/dashboard/admin.blade.php
+++ resources/views/dashboard/admin.blade.php
@@ -11,7 +11,6 @@
 @endsection
 
 @section('content')
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
     <!-- Welcome -->
     <div class="row">
         <div class="col-md-12">
