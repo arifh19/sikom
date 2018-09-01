0e21d10d4c0ace6590bba90279678bc54f5d1de7
Arif Hidayat <arif.h@mail.ugm.ac.id>
2018-09-01 09:33:16 +0700

    fix

--- app/Http/Controllers/ProposalsController.php
+++ app/Http/Controllers/ProposalsController.php
@@ -84,7 +84,7 @@ class ProposalsController extends Controller
             $html = $htmlBuilder
                 ->addColumn(['data' => 'action', 'name' => 'action', 'title' => 'Action', 'orderable' => false, 'searchable' => false])  
                 ->addColumn(['data' => 'judul', 'name' => 'judul', 'title' => 'Judul'])
-                ->addColumn(['data' => 'kategori.nama_kategori', 'name' => 'kategori.nama_kategori', 'title' => 'Kategori'])
+                ->addColumn(['data' => 'kategori.nama_kategori', 'name' => 'kategori_id', 'title' => 'Kategori'])
                 ->addColumn(['data' => 'user.name', 'name' => 'user.name', 'title' => 'Nama Tim'])
                 ->addColumn(['data' => 'updated_at', 'name' => 'updated_at', 'title' => 'Tanggal Input']);
                 
@@ -116,7 +116,7 @@ class ProposalsController extends Controller
             $html = $htmlBuilder
                 ->addColumn(['data' => 'action', 'name' => 'action', 'title' => 'Action', 'orderable' => false, 'searchable' => false])  
                 ->addColumn(['data' => 'judul', 'name' => 'judul', 'title' => 'Judul'])
-                ->addColumn(['data' => 'kategori.nama_kategori', 'name' => 'kategori.nama_kategori', 'title' => 'Kategori'])
+                ->addColumn(['data' => 'kategori.nama_kategori', 'name' => 'kategori_id', 'title' => 'Kategori'])
                 ->addColumn(['data' => 'user.name', 'name' => 'user.name', 'title' => 'Nama Tim'])
                 ->addColumn(['data' => 'updated_at', 'name' => 'updated_at', 'title' => 'Tanggal Input'])
                 ->addColumn(['data' => 'status', 'name' => 'status', 'title' => 'Status']);
@@ -156,7 +156,7 @@ class ProposalsController extends Controller
 
         $html = $htmlBuilder
             ->addColumn(['data' => 'action', 'name' => 'action', 'title' => 'Action', 'orderable' => false, 'searchable' => false])  
-            ->addColumn(['data' => 'kategori.nama_kategori', 'name' => 'kategori.nama_kategori', 'title' => 'Kategori'])
+            ->addColumn(['data' => 'kategori.nama_kategori', 'name' => 'kategori_id', 'title' => 'Kategori'])
             ->addColumn(['data' => 'judul', 'name' => 'judul', 'title' => 'Judul'])
             ->addColumn(['data' => 'user.name', 'name' => 'user.name', 'title' => 'Nama Tim'])
             ->addColumn(['data' => 'updated_at', 'name' => 'updated_at', 'title' => 'Tanggal Input'])
