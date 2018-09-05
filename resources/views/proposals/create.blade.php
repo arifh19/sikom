f6c085756362766a053943953da01b78833b14f4
Arif Hidayat <arif.h@mail.ugm.ac.id>
2018-09-05 10:23:01 +0700

    fix

--- resources/views/proposals/create.blade.php
+++ resources/views/proposals/create.blade.php
@@ -53,7 +53,7 @@ Proposal
                 <div class="box-header with-border">
                     <h3 class="box-title">Buku Panduan v.2.5</h3>
                 </div>
-                <center><iframe src ="{{ asset('/info/Buku_Panduan_GEMASTIK-11_Versi_2-5.pdf') }}" width="600px" height="400px"></iframe></center>
+                <center><iframe src ="{{ asset('/info/Buku_Panduan_GEMASTIK-11_Versi_2-6.pdf') }}" width="600px" height="400px"></iframe></center>
             </div>
         </div> 
         <!-- /.col (right)-->
